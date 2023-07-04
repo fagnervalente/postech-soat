58d80a6db1e1970eeb0210a6261d84ed28de0d25
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-07-04 00:46:25 -0300

    Adicionando validação do body de criação de cliente

--- src/adapter/http/routes/customerRoutes.ts
+++ src/adapter/http/routes/customerRoutes.ts
@@ -2,9 +2,12 @@ import { handleRequest } from '../Utils';
 import { CustomerController } from '../controllers/CustomerController';
 import { Router } from "express";
 
+import { validateSchema } from './schema/helper';
+import createUserBody from './schema/validation/createUserBody';
+
 const customerRoutes = Router();
 
 customerRoutes.get('/customer/:cpf', handleRequest(new CustomerController().getCustomerByCPF));
-customerRoutes.post('/customer', handleRequest(new CustomerController().create));
+customerRoutes.post('/customer', validateSchema(createUserBody), handleRequest(new CustomerController().create));
 
 export default customerRoutes;
