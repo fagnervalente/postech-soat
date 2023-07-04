901f2b3e36f40af6e16b8128d9e37deac77ea9ad
sirio-neto97 <sirio.neto97@gmail.com>
2023-07-03 22:52:59 -0300

    feat: test CreateUseCase_MISSING_REQUIRED_FIELDS

--- tests/integration/Customer.test.ts
+++ tests/integration/Customer.test.ts
@@ -3,24 +3,29 @@ import CustomerDatabaseRepository from '../../src/adapter/repository/CustomerDat
 import { AppDataSource } from '../../src/data-source';
 import { Customer } from '../../src/database/entities/Customer';
 
-beforeEach(async () => {
+beforeAll(async () => {
 	await AppDataSource.initialize();
 });
 
 describe('Test customer use cases', () => {
-	const mockedCustomer = {
-		name: 'Test customer',
-		cpf: '0000000000',
-		email: 'test@test.com'
-	} as Customer;
+	const mockedCustomer = { name: 'Test customer', cpf: '0000000000', email: 'test@test.com' } as Customer;
+	const customerRepository = new CustomerDatabaseRepository();
 
 	test('CreateUseCase', async () => {
-		const customerRepository = new CustomerDatabaseRepository();
 		const createUseCase = new CreateUseCase(customerRepository);
 		const created = await createUseCase.execute(mockedCustomer) as Customer;
-		mockedCustomer.id = created.id;
+		mockedCustomer.id = created?.id ?? 0;
 
 		expect(created).toEqual(mockedCustomer);
 		expect(createUseCase.hasErrors()).toBeFalsy();
+	});
+
+	test('CreateUseCase_MISSING_REQUIRED_FIELDS', async () => {
+		const invalidCustomer = { name: 'Test customer' } as Customer;
+		const createUseCase = new CreateUseCase(customerRepository);
+		const created = await createUseCase.execute(invalidCustomer) as Customer;
+
+		expect(created).toEqual(null);
+		expect(createUseCase.hasErrors()).toBeTruthy();
 	});
 });
