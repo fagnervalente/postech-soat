193e680436c4dcf554d38a7f59dedd04e99ad089
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-07-04 00:46:42 -0300

    Adicionando validação do body de checkout

--- src/adapter/http/routes/orderRoutes.ts
+++ src/adapter/http/routes/orderRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import OrderController from '../controllers/OrderController';
 import { handleRequest } from '../Utils';
 
+import { validateSchema } from "./schema/helper";
+import createOrderBody from "./schema/validation/createOrderBody";
+
 const orderRoutes = Router();
 const orderController = new OrderController();
 
-// orderRoutes.get('/order', orderController.getOrders);
 orderRoutes.get('/order', handleRequest(orderController.list));
-orderRoutes.post('/order/checkout', handleRequest(orderController.checkout));
+orderRoutes.post('/order/checkout', validateSchema(createOrderBody), handleRequest(orderController.checkout));
 
 export default orderRoutes;
