07759ce9df7a4653746812b92fa1a9c20484271b
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-07-06 22:50:22 -0300

    Added possibility for id to be nullable

--- src/core/application/useCase/Product/ProductDeleteUseCase.ts
+++ src/core/application/useCase/Product/ProductDeleteUseCase.ts
@@ -1,16 +1,15 @@
-import { Product } from "../../../../database/entities/Product";
 import ProductRepository from "../../ports/ProductRepository";
 import AbstractUseCase from "../AbstractUseCase";
 
-export default class ProductDeleteUseCase extends AbstractUseCase{
+export default class ProductDeleteUseCase extends AbstractUseCase {
 
 	constructor(readonly productRepository: ProductRepository) {
 		super(productRepository);
 	}
 
-	public async execute(id: number): Promise<void | null> {
+	public async execute(id: number | null): Promise<void | null> {
 		if (!id) {
-			this.setError({message: '"id" is required'});
+			this.setError({ message: '"id" is required' });
 			return null;
 		}
 		return await this.productRepository.delete(id);
