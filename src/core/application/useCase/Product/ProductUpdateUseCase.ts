de08be41782cebcf329e989442085771fb00ec91
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-07-07 23:15:25 -0300

    Adding product and category validation

--- src/core/application/useCase/Product/ProductUpdateUseCase.ts
+++ src/core/application/useCase/Product/ProductUpdateUseCase.ts
@@ -2,22 +2,68 @@ import { Product } from "../../../../database/entities/Product";
 import ProductRepository from "../../ports/ProductRepository";
 import AbstractUseCase from "../AbstractUseCase";
 import schema from "../../validation/updateProduct";
+import ProductCategoryFindByIdUseCase from "../ProductCategory/ProductCategoryFindByIdUseCase";
+import ProductCategoryRepository from "../../ports/ProductCategoryRepository";
+import { ProductCategory } from "../../../../database/entities/ProductCategory";
+import ProductFindByIdUseCase from "./ProductFindByIdUseCase";
 
-export default class ProductUpdateUseCase extends AbstractUseCase{
+export type ProductUpdateBody = {
+	id?: number;
+	name: string;
+	description: string;
+	price: number;
+	categoryId: number;
+};
 
-	constructor(readonly productRepository: ProductRepository) {
+export class ProductUpdateUseCase extends AbstractUseCase {
+	private productCategoryRepository: ProductCategoryRepository;
+
+	constructor(readonly productRepository: ProductRepository, productCategoryRepository: ProductCategoryRepository) {
 		super(productRepository);
+		this.productCategoryRepository = productCategoryRepository;
 	}
 
-	public async execute(product: Product): Promise<void> {
-		this.validateFields(product);
+	public async execute(body: ProductUpdateBody): Promise<void> {
+		this.validateFields(body);
+		let product = await this.validateProduct(body.id!);
+		let category = await this.validateCategory(body.categoryId);
 		if (this.hasErrors()) return;
 
+		product = {
+			id: product?.id,
+			name: body.name,
+			description: body.description,
+			price: body.price,
+			category: category!
+		};
 		await this.productRepository.update(product);
 	}
 
-		
-	private async validateFields(product: Product): Promise<void> {
+
+	private async validateFields(product: ProductUpdateBody): Promise<void> {
 		this.validateSchema(schema, product);
 	}
+
+	private async validateCategory(id: number): Promise<ProductCategory | null> {
+		const findCategory = new ProductCategoryFindByIdUseCase(this.productCategoryRepository);
+		const category = await findCategory.execute(id);
+
+		if (findCategory.hasErrors()) {
+			this.setErrors(findCategory.getErrors());
+		}
+
+		return category;
+	}
+
+	private async validateProduct(id: number): Promise<Product | null> {
+		const findProduct = new ProductFindByIdUseCase(this.productRepository);
+		const product = await findProduct.execute(id);
+
+		if (findProduct.hasErrors()) {
+			this.setErrors(findProduct.getErrors());
+		}
+
+		return product;
+	}
+
 }
--- src/core/application/validation/updateProduct.ts
+++ src/core/application/validation/updateProduct.ts
@@ -2,16 +2,21 @@ import { Schema } from "ajv";
 import { ajv } from "./helper";
 
 const schema: Schema = {
-  type: "object",
-  properties: {
-    id: { type: "integer" },
-    name: { type: "string" },
-    description: { type: "string" },
-    price: { type: "number", minimum: 0 },
-    categoryId: { type: "integer" },
-  },
-  required: ["id"],
-  additionalProperties: false
+	type: "object",
+	properties: {
+		id: { type: "integer", minimum: 0 },
+		name: { type: "string", minLength: 1 },
+		description: { type: "string", minLength: 1 },
+		price: { type: "number", minimum: 0 },
+		category: {
+			type: "object",
+			properties: {
+				id: { type: "integer", minimum: 0 },
+				name: { type: "string", minLength: 1 },
+			}
+		},
+	},
+	required: ["id"]
 }
 
 export default ajv.compile(schema);
