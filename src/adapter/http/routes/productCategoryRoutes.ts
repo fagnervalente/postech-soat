0921010a45257ee3565901080b4db5a8cc7692ea
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-07-08 16:42:06 -0300

    Removing use of handleRequest so that swagger correctly detects controllers

--- src/adapter/http/routes/productCategoryRoutes.ts
+++ src/adapter/http/routes/productCategoryRoutes.ts
@@ -1,13 +1,12 @@
 import { ProductCategoryController } from './../controllers/ProductCategoryController';
 import { Router } from "express";
-import { handleRequest } from '../Utils';
 
 const productCategoryRoutes = Router();
 
-productCategoryRoutes.post('/category', handleRequest(new ProductCategoryController().create));
-productCategoryRoutes.get('/category/:id', handleRequest(new ProductCategoryController().getById));
-productCategoryRoutes.get('/category/', handleRequest(new ProductCategoryController().list));
-productCategoryRoutes.put('/category/:id', handleRequest(new ProductCategoryController().update));
-productCategoryRoutes.delete('/category/:id', handleRequest(new ProductCategoryController().delete));
+productCategoryRoutes.get('/category', new ProductCategoryController().list);
+productCategoryRoutes.get('/category/:id', new ProductCategoryController().getById);
+productCategoryRoutes.post('/category', new ProductCategoryController().create);
+productCategoryRoutes.put('/category/:id', new ProductCategoryController().update);
+productCategoryRoutes.delete('/category/:id', new ProductCategoryController().delete);
 
 export default productCategoryRoutes;
