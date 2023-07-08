3bf7e0449cfdac3556138bab4003533f7db9751f
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-07-08 16:42:33 -0300

    Removing unused parameters

--- src/core/application/validation/updateProductCategory.ts
+++ src/core/application/validation/updateProductCategory.ts
@@ -2,13 +2,12 @@ import { Schema } from "ajv";
 import { ajv } from "./helper";
 
 const schema: Schema = {
-  type: "object",
-  properties: {
-    id: { type: "integer" },
-    name: { type: "string" }
-  },
-  required: ["id", "name"],
-  additionalProperties: false
+	type: "object",
+	properties: {
+		name: { type: "string" }
+	},
+	required: ["name"],
+	additionalProperties: false
 }
 
 export default ajv.compile(schema);
