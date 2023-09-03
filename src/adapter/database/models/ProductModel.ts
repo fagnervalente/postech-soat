023bfb169937a08b9da3b254aa2cb766df6f0919
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-09-03 16:14:01 -0300

    Removing orders from the constructor

--- src/adapter/database/models/ProductModel.ts
+++ src/adapter/database/models/ProductModel.ts
@@ -4,31 +4,30 @@ import { OrderModel } from "./OrderModel";
 
 @Entity('products')
 export class ProductModel {
-  @PrimaryGeneratedColumn()
-  id?: number;
+	@PrimaryGeneratedColumn()
+	id?: number;
 
-  @Column({ type: 'text', nullable: false })
-  name: string;
+	@Column({ type: 'text', nullable: false })
+	name: string;
 
-  @Column({ type: 'text', nullable: false })
-  description: string;
+	@Column({ type: 'text', nullable: false })
+	description: string;
 
-  @Column({ type: 'decimal', precision: 15, scale: 2, nullable: false })
-  price: number;
+	@Column({ type: 'decimal', precision: 15, scale: 2, nullable: false })
+	price: number;
 
-  @ManyToOne(() => ProductCategoryModel, category => category.products)
-  @JoinColumn({ name: 'category_id' })
-  category: ProductCategoryModel
+	@ManyToOne(() => ProductCategoryModel, category => category.products)
+	@JoinColumn({ name: 'category_id' })
+	category: ProductCategoryModel;
 
-  @ManyToMany(() => OrderModel, order => order.products)
-  orders?: OrderModel[]
+	@ManyToMany(() => OrderModel, order => order.products)
+	orders?: OrderModel[];
 
-	constructor(id: number | undefined, name: string, description: string, price: number, category: ProductCategoryModel, orders: OrderModel[]) {
+	constructor(id: number | undefined, name: string, description: string, price: number, category: ProductCategoryModel) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.category = category;
-		this.orders = orders;
 	}
 }
