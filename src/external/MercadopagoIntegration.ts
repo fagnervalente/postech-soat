fc00b9ab3602655284a463062e69032b583d9fc4
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2023-09-03 22:23:51 -0300

    Changing usage of node-fetch v3 to v2

--- src/external/MercadopagoIntegration.ts
+++ src/external/MercadopagoIntegration.ts
@@ -1,36 +1,36 @@
-import fetch from 'node-fetch';
+const fetch = require("node-fetch");
 import IPaymentAPIIntegration from "../ports/PaymentAPI/IPaymentAPIIntegration";
 
 const accessToken = process.env.MERCADO_PAGO_ACCESS_TOKEN;
 
-export default class MercadopagoIntegration implements IPaymentAPIIntegration{
-    _paymentId: number;
+export default class MercadopagoIntegration implements IPaymentAPIIntegration {
+	_paymentId: number;
 
-    constructor(webhookNotification: any){
-        this._paymentId = Number( webhookNotification.data?.id );
-    }
+	constructor(webhookNotification: any) {
+		this._paymentId = Number(webhookNotification.data?.id);
+	}
 
-    async getPayment(): Promise<any> {
-        try {
-            const url = `https://api.mercadopago.com/v1/payments/${this._paymentId}`;
-            const headers = {
-              Authorization: `Bearer ${accessToken}`,
-            };
-      
-            const response = await fetch(url, { headers });
-      
-            if (!response.ok) {
-              console.log(`Erro ao consultar pagamento: ${response.statusText}`);
-              return null;
-            }
-      
-            const payment = await response.json();
-            return payment;
+	async getPayment(): Promise<any> {
+		try {
+			const url = `https://api.mercadopago.com/v1/payments/${this._paymentId}`;
+			const headers = {
+				Authorization: `Bearer ${accessToken}`,
+			};
 
-          } catch (error) {
-            console.log(`Erro ao consultar pagamento`, error);
-            return null;
-          }
-    }
+			const response = await fetch(url, { headers });
+
+			if (!response.ok) {
+				console.log(`Erro ao consultar pagamento: ${response.statusText}`);
+				return null;
+			}
+
+			const payment = await response.json();
+			return payment;
+
+		} catch (error) {
+			console.log(`Erro ao consultar pagamento`, error);
+			return null;
+		}
+	}
 
 }
