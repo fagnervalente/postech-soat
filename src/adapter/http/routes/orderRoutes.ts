7b1b75dc7e5fcf7658e2ad92ee7b267f3bd3f5de
augustoefr <augustoefr@gmail.com>
2023-11-05 23:33:59 -0300

    Integration with lambda authentication

--- src/adapter/http/routes/orderRoutes.ts
+++ src/adapter/http/routes/orderRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import HttpUtils from "../HttpUtils";
 import OrderAPIController from "../controllers/OrderAPIController";
+import auth from "../../auth/authMiddleware";
 
 const orderRoutes = HttpUtils.asyncRouterHandler(Router());
 
 orderRoutes.get('/order', new OrderAPIController().list);
-orderRoutes.post('/order/checkout', new OrderAPIController().checkout);
+orderRoutes.post('/order/checkout', auth, new OrderAPIController().checkout);
 orderRoutes.get('/order/payment/:id', new OrderAPIController().getPaymentStatus);
 
 orderRoutes.post('/order/payment/:id', new OrderAPIController().handlePaymentWebhook);
