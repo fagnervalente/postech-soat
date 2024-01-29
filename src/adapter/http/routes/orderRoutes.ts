7688e678394a013df7b2b003930a6f1924870287
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2024-01-28 23:31:38 -0300

    Configuring port to access order endpoint

--- src/adapter/http/routes/orderRoutes.ts
+++ src/adapter/http/routes/orderRoutes.ts
@@ -5,10 +5,11 @@ import auth from "../../auth/authMiddleware";
 import fetch from "node-fetch";
 
 const orderRoutes = HttpUtils.asyncRouterHandler(Router());
+const port = process.env.SERVER_PORT;
 const orderEndpoint = process.env.ORDER_SERVICE_ENDPOINT as string;
 
 orderRoutes.get('/order', async () => {
-	const response = await fetch(`${orderEndpoint}/order`, {
+	const response = await fetch(`${orderEndpoint}:${port}/order`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
@@ -18,7 +19,7 @@ orderRoutes.get('/order', async () => {
 });
 
 orderRoutes.post('/order/checkout', auth, async (req, _) => {
-	const response = await fetch(`${orderEndpoint}/order/checkout`, {
+	const response = await fetch(`${orderEndpoint}:${port}/order/checkout`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -30,7 +31,7 @@ orderRoutes.post('/order/checkout', auth, async (req, _) => {
 
 orderRoutes.get('/order/payment/:id', async (req, _) => {
 	const id = Number(req.params.id)
-	const response = await fetch(`${orderEndpoint}/order/payment/${id}`, {
+	const response = await fetch(`${orderEndpoint}:${port}/order/payment/${id}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
@@ -41,7 +42,7 @@ orderRoutes.get('/order/payment/:id', async (req, _) => {
 
 orderRoutes.put('/order/status/:id', async (req, _) => {
 	const id = Number(req.params.id)
-	const response = await fetch(`${orderEndpoint}/order/status/${id}`, {
+	const response = await fetch(`${orderEndpoint}:${port}/order/status/${id}`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
