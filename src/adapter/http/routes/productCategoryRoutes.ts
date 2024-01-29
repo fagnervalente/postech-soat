ebb302a8f25bccef4e6230c7a101354944f735f7
augustoefr <augustoefr@gmail.com>
2024-01-29 00:16:00 -0300

    User and product services integration

--- src/adapter/http/routes/productCategoryRoutes.ts
+++ src/adapter/http/routes/productCategoryRoutes.ts
@@ -1,13 +1,46 @@
-import ProductCategoryAPIController  from '../controllers/ProductCategoryAPIController';
 import { Router } from "express";
 import HttpUtils from "../HttpUtils";
+import fetch from 'node-fetch';
 
 const productCategoryRoutes = HttpUtils.asyncRouterHandler(Router());
+const productEndpoint = process.env.PRODUCT_SERVICE_ENDPOINT as string;
+const requestInit = {
+    method: 'GET',
+    headers: {'Content-Type':'application/json'}
+};
 
-productCategoryRoutes.get('/category', new ProductCategoryAPIController().list);
-productCategoryRoutes.get('/category/:id', new ProductCategoryAPIController().getById);
-productCategoryRoutes.post('/category', new ProductCategoryAPIController().create);
-productCategoryRoutes.put('/category/:id', new ProductCategoryAPIController().update);
-productCategoryRoutes.delete('/category/:id', new ProductCategoryAPIController().delete);
+productCategoryRoutes.get('/category', async ()=>{
+    return await fetch(`${productEndpoint}/category`, requestInit);
+});
+
+productCategoryRoutes.get('/category/:id', async (req, _)=>{
+    const {id} = req.params;
+    return await fetch(`${productEndpoint}/category/${id}`, requestInit);
+});
+
+productCategoryRoutes.post('/category', async (req, _)=>{
+    return await fetch(`${productEndpoint}/category`, {
+        ...requestInit,
+        method: 'POST',
+        body: JSON.stringify(req.body)
+    });
+});
+
+productCategoryRoutes.put('/category/:id', async (req, _)=>{
+    const {id} = req.params;
+    return await fetch(`${productEndpoint}/category/${id}`, {
+        ...requestInit,
+        method: 'PUT',
+        body: JSON.stringify(req.body)
+    });
+});
+
+productCategoryRoutes.delete('/category/:id', async (req, _)=>{
+    const {id} = req.params;
+    return await fetch(`${productEndpoint}/category/${id}`, {
+        ...requestInit,
+        method: 'DELETE'
+    });
+});
 
 export default productCategoryRoutes;
--- src/ports/ICustomerRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Customer } from "@entities/Customer";
-
-export default interface ICustomerRepository {
-	save(customer: Customer): Promise<Customer>;
-	findByCPF(cpf: string): Promise<Customer | null>;
-	delete(id: number): Promise<void>;
-	list(): Promise<Customer[] | null>;
-}
