4f9f5531846df33c69228c159b755a5a3b32d747
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2024-01-29 14:53:56 -0300

    Corrigindo endpoints

--- src/adapter/http/routes/orderRoutes.ts
+++ src/adapter/http/routes/orderRoutes.ts
@@ -8,7 +8,7 @@ const orderRoutes = HttpUtils.asyncRouterHandler(Router());
 const orderEndpoint = process.env.ORDER_SERVICE_ENDPOINT as string;
 
 orderRoutes.get('/order', async () => {
-	const response = await fetch(`${orderEndpoint}/order`, {
+	const response = await fetch(`${orderEndpoint}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
@@ -18,7 +18,7 @@ orderRoutes.get('/order', async () => {
 });
 
 orderRoutes.post('/order/checkout', auth, async (req, _) => {
-	const response = await fetch(`${orderEndpoint}/order/checkout`, {
+	const response = await fetch(`${orderEndpoint}/checkout`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -30,7 +30,7 @@ orderRoutes.post('/order/checkout', auth, async (req, _) => {
 
 orderRoutes.get('/order/payment/:id', async (req, _) => {
 	const id = Number(req.params.id)
-	const response = await fetch(`${orderEndpoint}/order/payment/${id}`, {
+	const response = await fetch(`${orderEndpoint}/payment/${id}`, {
 		method: 'GET',
 		headers: {
 			'Content-Type': 'application/json',
@@ -41,8 +41,8 @@ orderRoutes.get('/order/payment/:id', async (req, _) => {
 
 orderRoutes.put('/order/status/:id', async (req, _) => {
 	const id = Number(req.params.id)
-	const response = await fetch(`${orderEndpoint}/order/status/${id}`, {
-		method: 'POST',
+	const response = await fetch(`${orderEndpoint}/status/${id}`, {
+		method: 'PUT',
 		headers: {
 			'Content-Type': 'application/json',
 		},
