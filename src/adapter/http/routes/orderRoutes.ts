e045e7161e420f4d45be7bdf77e296bf4d159752
AndersonBarbosaDeFreitas <anderson.barbosa@dcx.ufpb.br>
2024-01-28 22:09:17 -0300

    Starting to adjust integration with the Order service

--- src/adapter/http/routes/orderRoutes.ts
+++ src/adapter/http/routes/orderRoutes.ts
@@ -2,14 +2,53 @@ import { Router } from "express";
 import HttpUtils from "../HttpUtils";
 import OrderAPIController from "../controllers/OrderAPIController";
 import auth from "../../auth/authMiddleware";
+import fetch from "node-fetch";
 
 const orderRoutes = HttpUtils.asyncRouterHandler(Router());
+const orderEndpoint = process.env.ORDER_SERVICE_ENDPOINT as string;
 
-orderRoutes.get('/order', new OrderAPIController().list);
-orderRoutes.post('/order/checkout', auth, new OrderAPIController().checkout);
-orderRoutes.get('/order/payment/:id', new OrderAPIController().getPaymentStatus);
+orderRoutes.get('/order', async () => {
+	const response = await fetch(`${orderEndpoint}/order`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	return response;
+});
 
-orderRoutes.post('/order/payment/:id', new OrderAPIController().handlePaymentWebhook);
-orderRoutes.put('/order/status/:id', new OrderAPIController().updateStatus);
+orderRoutes.post('/order/checkout', auth, async (req, _) => {
+	const response = await fetch(`${orderEndpoint}/order/checkout`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(req.body)
+	});
+	return response;
+});
+
+orderRoutes.get('/order/payment/:id', async (req, _) => {
+	const id = Number(req.params.id)
+	const response = await fetch(`${orderEndpoint}/order/payment/${id}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	return response;
+});
+
+orderRoutes.put('/order/status/:id', async (req, _) => {
+	const id = Number(req.params.id)
+	const response = await fetch(`${orderEndpoint}/order/status/${id}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(req.body)
+	});
+	return response;
+});
 
 export default orderRoutes;
