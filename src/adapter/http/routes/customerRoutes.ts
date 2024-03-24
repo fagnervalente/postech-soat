10e96239a74affebe79d057733faf23879584c3c
Fagner Valente <fagnervalente@gmail.com>
2024-03-23 23:51:06 -0300

    Update Endpoints

--- src/adapter/http/routes/customerRoutes.ts
+++ src/adapter/http/routes/customerRoutes.ts
@@ -4,21 +4,21 @@ import HttpUtils from '../HttpUtils';
 import got from "got";
 
 const customerRoutes = HttpUtils.asyncRouterHandler(Router());
-const customerEndpoint = process.env.CUSTOMER_SERVICE_ENDPOINT as string;
+const CUSTOMER_SERVICE_ENDPOINT = process.env.CUSTOMER_SERVICE_ENDPOINT as string;
 
 customerRoutes.get('/customer', async (_, res) => {
-	const response = await got.get(customerEndpoint);
+	const response = await got.get(CUSTOMER_SERVICE_ENDPOINT);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
 customerRoutes.get('/customer/:cpf', async (req, res) => {
 	const { cpf } = req.params;
-	const response = await got.get(`${customerEndpoint}/${cpf}`);
+	const response = await got.get(`${CUSTOMER_SERVICE_ENDPOINT}/${cpf}`);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
 customerRoutes.post('/customer', async (req, res) => {
-	const response = await got.post(customerEndpoint, { json: req.body });
+	const response = await got.post(CUSTOMER_SERVICE_ENDPOINT, { json: req.body });
 	const authIntegration = new AuthLambdaIntegration();
 	await authIntegration.putClient(JSON.parse(response.body) as Customer);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
@@ -26,7 +26,7 @@ customerRoutes.post('/customer', async (req, res) => {
 
 customerRoutes.delete('/customer/:id', async (req, res) => {
 	const { id } = req.params;
-	const response = await got.delete(`${customerEndpoint}/${id}`);
+	const response = await got.delete(`${CUSTOMER_SERVICE_ENDPOINT}/${id}`);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
--- src/adapter/http/routes/productCategoryRoutes.ts
+++ src/adapter/http/routes/productCategoryRoutes.ts
@@ -3,33 +3,33 @@ import HttpUtils from "../HttpUtils";
 import got from "got";
 
 const productCategoryRoutes = HttpUtils.asyncRouterHandler(Router());
-const productEndpoint = process.env.PRODUCT_SERVICE_ENDPOINT as string;
+const PRODUCT_SERVICE_ENDPOINT = process.env.PRODUCT_SERVICE_ENDPOINT as string;
 
 productCategoryRoutes.get('/category', async (_, res) => {
-	const response = await got.get(`${productEndpoint}/category`);
+	const response = await got.get(`${PRODUCT_SERVICE_ENDPOINT}/category`);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
 productCategoryRoutes.get('/category/:id', async (req, res) => {
 	const { id } = req.params;
-	const response = await got.get(`${productEndpoint}/category/${id}`);
+	const response = await got.get(`${PRODUCT_SERVICE_ENDPOINT}/category/${id}`);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
 productCategoryRoutes.post('/category', async (req, res) => {
-	const response = await got.post(`${productEndpoint}/category`, { json: req.body });
+	const response = await got.post(`${PRODUCT_SERVICE_ENDPOINT}/category`, { json: req.body });
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
 productCategoryRoutes.put('/category/:id', async (req, res) => {
 	const { id } = req.params;
-	const response = await got.put(`${productEndpoint}/category/${id}`, { json: req.body });
+	const response = await got.put(`${PRODUCT_SERVICE_ENDPOINT}/category/${id}`, { json: req.body });
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
 productCategoryRoutes.delete('/category/:id', async (req, res) => {
 	const { id } = req.params;
-	const response = await got.delete(`${productEndpoint}/category/${id}`);
+	const response = await got.delete(`${PRODUCT_SERVICE_ENDPOINT}/category/${id}`);
 	return res.status(response.statusCode).json(JSON.parse(response.body));
 });
 
